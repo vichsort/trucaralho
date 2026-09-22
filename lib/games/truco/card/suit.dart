16b2a1c0021c28fdb52bc37814a50b7170942524
Vitor Mignoni <vichsort@proton.me>
2026-09-22 10:24:18 -0300

    feat: close card and core rule foundation

--- lib/games/truco/card/suit.dart
+++ lib/games/truco/card/suit.dart
@@ -4,7 +4,14 @@ enum Suit {
   hearts,
   clubs;
 
-  int get manilhaStrength => index;
+  /// Strength used when two manilhas have the same rank.
+  /// Paulista order: Ouros < Espadas < Copas < Paus.
+  int get manilhaStrength => switch (this) {
+        Suit.diamonds => 0,
+        Suit.spades => 1,
+        Suit.hearts => 2,
+        Suit.clubs => 3,
+      };
 
   String get symbol => switch (this) {
         Suit.diamonds => '♦',
