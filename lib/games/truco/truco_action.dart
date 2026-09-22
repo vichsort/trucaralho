7063b32712af97e06fc8746a6006c9a09f234c84
Vitor Mignoni <vichsort@proton.me>
2026-09-22 08:57:45 -0300

    feat: model truco raises

--- lib/games/truco/truco_action.dart
+++ lib/games/truco/truco_action.dart
@@ -8,25 +8,32 @@ sealed class TrucoAction implements GameAction {
 final class PlayCard extends TrucoAction {
   final String playerId;
   final Card card;
-  const PlayCard({required this.playerId, required this.card});
-  @override String get type => 'play_card';
+  const PlayCard({required this.playerId,required this.card});
+  @override String get type=>'play_card';
 }
 
 final class RequestTruco extends TrucoAction {
   final String playerId;
   final int requestedValue;
-  const RequestTruco({required this.playerId, required this.requestedValue});
-  @override String get type => 'request_truco';
+  const RequestTruco({required this.playerId,required this.requestedValue});
+  @override String get type=>'request_truco';
+}
+
+final class RaiseTruco extends TrucoAction {
+  final String playerId;
+  final int requestedValue;
+  const RaiseTruco({required this.playerId,required this.requestedValue});
+  @override String get type=>'raise_truco';
 }
 
 final class AcceptTruco extends TrucoAction {
   final String playerId;
   const AcceptTruco(this.playerId);
-  @override String get type => 'accept_truco';
+  @override String get type=>'accept_truco';
 }
 
 final class FoldTruco extends TrucoAction {
   final String playerId;
   const FoldTruco(this.playerId);
-  @override String get type => 'fold_truco';
+  @override String get type=>'fold_truco';
 }
