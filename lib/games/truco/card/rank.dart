de297c8c6e7c554a839f06449c6cb5803b5871f8
Vitor Mignoni <vichsort@proton.me>
2026-09-22 10:24:14 -0300

    feat: close card and core rule foundation

--- lib/games/truco/card/rank.dart
+++ lib/games/truco/card/rank.dart
@@ -23,6 +23,20 @@ enum Rank {
         Rank.three => '3',
       };
 
+  /// Strength used for non-manilha cards. Higher is stronger.
+  int get strength => switch (this) {
+        Rank.four => 0,
+        Rank.five => 1,
+        Rank.six => 2,
+        Rank.seven => 3,
+        Rank.queen => 4,
+        Rank.jack => 5,
+        Rank.king => 6,
+        Rank.ace => 7,
+        Rank.two => 8,
+        Rank.three => 9,
+      };
+
   static Rank fromSymbol(String value) => values.firstWhere(
         (rank) => rank.symbol == value.toUpperCase(),
         orElse: () => throw ArgumentError('Rank inválido: $value'),
